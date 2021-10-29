a7346b1d62252c2a18dd77e9f37f5a3aac19e28d
Ubuntu <ubuntu@ip-172-31-43-203.sa-east-1.compute.internal>
2021-10-29 19:07:26 +0000

    init

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const http = require('http');
+const app = express();
+
+const https = require('https');
+const fs = require('fs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+var cors = require('cors');
+
+// app.use(express.vhost('checkout.hscp.cl', require('/home/administrador/checkOut/web').app));
+// app.use(express.vhost('checkin.hscp.cl', require(__dirname+'Prod/index.html').app));
+
+app.use(cors({origin: 'http://localhost:5050'}));
+app.use(express.static(path.join(__dirname, 'Prod')));
+app.use(express.static(path.join('/home/administrador/checkOut/web')));
+app.use(express.static(path.join('/home/administrador/frenonApi/img')));
+
+app.get('*', (req, res) => {
+	console.log("data",req.headers.host);
+	res.sendFile(path.join(__dirname, 'Prod/index.html'));
+});
+
+
+const port = '5050';
+
+const server = http.createServer(app);
+
+
+server.listen(port,() => console.log(`web check in-out:${port}`));
